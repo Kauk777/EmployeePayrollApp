1781036ac0678f56cda1fecb4abcd94304c7350a
Kauk777 <curokky777@gmail.com>
2020-11-19 22:09:28 +0530

    [KAUSHAL]Added: UC7 UX design to set button style

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -2,4 +2,9 @@ function displaySalary() {
     const salary=document.getElementById('salary');
     const output=document.querySelector('.salary-output')
     output.textContent=salary.value;
+
+    // By eventListener
+    /*salary.addEventListener('input', function() {
+        output.textContent=salary.value;
+    });*/
 }
