e0f2b216094ce28e4d9b655742392d0e689e935f
Kauk777 <curokky777@gmail.com>
2020-11-19 22:13:34 +0530

    [KAUSHAL]Added: UC8 Added eventListener to salary element

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,10 +1,5 @@
-function displaySalary() {
-    const salary=document.getElementById('salary');
-    const output=document.querySelector('.salary-output')
-    output.textContent=salary.value;
-
-    // By eventListener
-    /*salary.addEventListener('input', function() {
-        output.textContent=salary.value;
-    });*/
-}
+const salary = document.getElementById('salary');
+const output = document.querySelector('.salary-output');
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
